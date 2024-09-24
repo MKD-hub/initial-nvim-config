dd8596e263cfa6715af95e6223c9f24f91238b70
MKD-hub <mkdiab58@gmail.com>
2024-09-24 12:18:31 +0300

    added pasting from clipboard

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -2,5 +2,14 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=3")
 vim.cmd("set softtabstop=3")
 vim.cmd("set shiftwidth=3")
+vim.keymap.set("", "<Space>", "<Nop>")
 vim.g.mapleader = " "
+vim.keymap.set(
+   'n', 
+   '<leader>v', 
+   vim.cmd([[
+      let @" = system('xclip -o')
+   ]]), -- using xclip to copy to the clipboard at register @" the default register
+   {}
+);
 
